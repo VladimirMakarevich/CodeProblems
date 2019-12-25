62ae27af182a6af39586bb549ec93b2b96694ed0
Vladimir Makarevich <makarevich.dev@outlook.com>
2019-12-25 21:41:36 +0300

    problem2: add init class

---
+++ CodeProblems/ProblemBook/Problem2.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace CodeProblems.ProblemBook {
+    public class Problem2 : ProblemBase {
+        public override void Execute() {
+            Console.ReadLine();
+        }
+    }
+}
