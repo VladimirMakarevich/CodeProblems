471ef54fda75136fd17edabbf2b0d0e87ae2a862
Vladimir Makarevich <makarevich.dev@outlook.com>
2019-12-26 23:02:34 +0300

    problem3: add implementation

--- CodeProblems/ProblemBook/Problem3.cs
+++ CodeProblems/ProblemBook/Problem3.cs
@@ -3,7 +3,43 @@
 namespace CodeProblems.ProblemBook {
     public class Problem3 : ProblemBase {
         public override void Execute() {
+            new Baz();
+
             Console.ReadLine();
+        }
+    }
+
+    public class Foo {
+        public Foo() {
+            Qux();
+        }
+
+        public virtual void Qux() {
+            Console.WriteLine("Foo.Qux()");
+        }
+    }
+
+    public class Bar : Foo {
+        protected string name;
+
+        public Bar() {
+            name = "Bar";
+        }
+
+        public override void Qux() {
+            Console.WriteLine("Bar.Qux(), " + name);
+        }
+
+        public void Qux(params object[] args) {
+            Console.WriteLine("Bar.Qux(params object[])");
+        }
+    }
+
+    public class Baz : Bar {
+        public Baz() {
+            name = "Baz";
+            Qux();
+            ((Foo) this).Qux();
         }
     }
 }
