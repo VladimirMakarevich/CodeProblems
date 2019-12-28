a56d836352a47f213115c5bc67bb70fe65112067
Vladimir Makarevich <makarevich.dev@outlook.com>
2019-12-28 22:00:20 +0300

    problem4: add code

---
+++ CodeProblems/ProblemBook/Problem4.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace CodeProblems.ProblemBook {
+    public class Problem4 : ProblemBase {
+        public override void Execute() {
+            new Baz();
+        }
+
+        public class Foo {
+            public Foo() {
+                Qux();
+            }
+
+            public virtual void Qux() {
+                Console.WriteLine("Foo.Qux()");
+            }
+        }
+
+        public class Bar : Foo {
+            protected string name;
+
+            public Bar() {
+                name = "Bar";
+            }
+
+            public override void Qux() {
+                Console.WriteLine("Bar.Qux(), " + name);
+            }
+
+            public void Qux(params object[] args) {
+                Console.WriteLine("Bar.Qux(params object[])");
+            }
+        }
+
+        public class Baz : Bar {
+            public Baz() {
+                name = "Baz";
+                Qux();
+                ((Foo) this).Qux();
+            }
+        }
+    }
+}
