0ff69f3921514757fe6ae5984c9dbaec9ff0fc36
Vladimir Makarevich <makarevich.dev@outlook.com>
2020-01-08 18:12:08 +0300

    add 5th problem

---
+++ CodeProblems/ProblemBook/Problem5.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CodeProblems.ProblemBook {
+    public class Problem5 : ProblemBase {
+        public override void Execute() {
+            Console.WriteLine($"\n{nameof(Problem5)} start >>>>>>>>>>>> ");
+
+            Foo<int>.Bar++;
+            Console.WriteLine(Foo<double>.Bar);
+
+            Console.WriteLine($"{nameof(Problem5)} end <<<<<<<<<<<< \n");
+            Console.ReadKey();
+        }
+
+        public class Foo<T> {
+            public static int Bar;
+        }
+    }
+}
